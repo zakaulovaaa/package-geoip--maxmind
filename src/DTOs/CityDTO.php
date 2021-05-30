56f555497217e39fc1756d78087203fd36d9c331
zakaulovaaa <darya.zakaulova@yandex.ru>
2021-05-30 12:35:37 +0800

    Добавляет классы DTO, gitignore, composer.json

---
+++ src/DTOs/CityDTO.php
@@ -0,0 +1,270 @@
+<?php
+
+
+namespace GeoIp\DTO;
+
+
+class CityDTO
+{
+    private string $geonameId;
+    private ?string $localeCode;
+    private ?string $continentCode;
+    private ?string $continentName;
+    private ?string $countryIsoCode;
+    private ?string $countryName;
+    private ?string $subdivision1IsoCode;
+    private ?string $subdivision1Name;
+    private ?string $subdivision2IsoCode;
+    private ?string $subdivision2Name;
+    private ?string $cityName;
+    private ?string $metroCode;
+    private ?string $timeZone;
+    private ?string $isInEuropeanUnion;
+
+    public function __construct($geonameId, $localeCode, $continentCode, $continentName, $countryIsoCode,
+                                $countryName, $subdivision1IsoCode, $subdivision1Name, $subdivision2IsoCode,
+                                $subdivision2Name, $cityName, $metroCode, $timeZone, $isInEuropeanUnion)
+    {
+        $this->geonameId = $geonameId;
+        $this->localeCode = $localeCode;
+        $this->continentCode = $continentCode;
+        $this->continentName = $continentName;
+        $this->countryIsoCode = $countryIsoCode;
+        $this->countryName = $countryName;
+        $this->subdivision1IsoCode = $subdivision1IsoCode;
+        $this->subdivision1Name = $subdivision1Name;
+        $this->subdivision2IsoCode = $subdivision2IsoCode;
+        $this->subdivision2Name = $subdivision2Name;
+        $this->cityName = $cityName;
+        $this->metroCode = $metroCode;
+        $this->timeZone = $timeZone;
+        $this->isInEuropeanUnion = $isInEuropeanUnion;
+    }
+
+
+
+    /**
+     * @return string
+     */
+    public function getGeonameId(): string
+    {
+        return $this->geonameId;
+    }
+
+    /**
+     * @param string $geonameId
+     */
+    public function setGeonameId(string $geonameId): void
+    {
+        $this->geonameId = $geonameId;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLocaleCode(): ?string
+    {
+        return $this->localeCode;
+    }
+
+    /**
+     * @param string|null $localeCode
+     */
+    public function setLocaleCode(?string $localeCode): void
+    {
+        $this->localeCode = $localeCode;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getContinentCode(): ?string
+    {
+        return $this->continentCode;
+    }
+
+    /**
+     * @param string|null $continentCode
+     */
+    public function setContinentCode(?string $continentCode): void
+    {
+        $this->continentCode = $continentCode;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getContinentName(): ?string
+    {
+        return $this->continentName;
+    }
+
+    /**
+     * @param string|null $continentName
+     */
+    public function setContinentName(?string $continentName): void
+    {
+        $this->continentName = $continentName;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCountryIsoCode(): ?string
+    {
+        return $this->countryIsoCode;
+    }
+
+    /**
+     * @param string|null $countryIsoCode
+     */
+    public function setCountryIsoCode(?string $countryIsoCode): void
+    {
+        $this->countryIsoCode = $countryIsoCode;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCountryName(): ?string
+    {
+        return $this->countryName;
+    }
+
+    /**
+     * @param string|null $countryName
+     */
+    public function setCountryName(?string $countryName): void
+    {
+        $this->countryName = $countryName;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSubdivision1IsoCode(): ?string
+    {
+        return $this->subdivision1IsoCode;
+    }
+
+    /**
+     * @param string|null $subdivision1IsoCode
+     */
+    public function setSubdivision1IsoCode(?string $subdivision1IsoCode): void
+    {
+        $this->subdivision1IsoCode = $subdivision1IsoCode;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSubdivision1Name(): ?string
+    {
+        return $this->subdivision1Name;
+    }
+
+    /**
+     * @param string|null $subdivision1Name
+     */
+    public function setSubdivision1Name(?string $subdivision1Name): void
+    {
+        $this->subdivision1Name = $subdivision1Name;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSubdivision2IsoCode(): ?string
+    {
+        return $this->subdivision2IsoCode;
+    }
+
+    /**
+     * @param string|null $subdivision2IsoCode
+     */
+    public function setSubdivision2IsoCode(?string $subdivision2IsoCode): void
+    {
+        $this->subdivision2IsoCode = $subdivision2IsoCode;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSubdivision2Name(): ?string
+    {
+        return $this->subdivision2Name;
+    }
+
+    /**
+     * @param string|null $subdivision2Name
+     */
+    public function setSubdivision2Name(?string $subdivision2Name): void
+    {
+        $this->subdivision2Name = $subdivision2Name;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCityName(): ?string
+    {
+        return $this->cityName;
+    }
+
+    /**
+     * @param string|null $cityName
+     */
+    public function setCityName(?string $cityName): void
+    {
+        $this->cityName = $cityName;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getMetroCode(): ?string
+    {
+        return $this->metroCode;
+    }
+
+    /**
+     * @param string|null $metroCode
+     */
+    public function setMetroCode(?string $metroCode): void
+    {
+        $this->metroCode = $metroCode;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTimeZone(): ?string
+    {
+        return $this->timeZone;
+    }
+
+    /**
+     * @param string|null $timeZone
+     */
+    public function setTimeZone(?string $timeZone): void
+    {
+        $this->timeZone = $timeZone;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getIsInEuropeanUnion(): ?string
+    {
+        return $this->isInEuropeanUnion;
+    }
+
+    /**
+     * @param string|null $isInEuropeanUnion
+     */
+    public function setIsInEuropeanUnion(?string $isInEuropeanUnion): void
+    {
+        $this->isInEuropeanUnion = $isInEuropeanUnion;
+    }
+
+}
---
+++ src/DTOs/IpDTO.php
@@ -0,0 +1,247 @@
+<?php
+
+
+namespace GeoIp\DTO;
+
+
+class IpDTO
+{
+    private string $network;
+    private string $geonameId;
+    private ?string $registeredCountryGeonameId;
+    private ?string $representedCountryGeonameId;
+    private ?string $isAnonymousProxy;
+    private ?string $isSatelliteProvider;
+    private ?string $postalCode;
+    private ?string $latitude;
+    private ?string $longitude;
+    private ?string $accuracyRadius;
+    private ?string $networkFrom;
+    private ?string $networkTo;
+
+    /**
+     * IpDTO constructor.
+     * @param string $network
+     * @param string $geonameId
+     * @param string|null $registeredCountryGeonameId
+     * @param string|null $representedCountryGeonameId
+     * @param string|null $isAnonymousProxy
+     * @param string|null $isSatelliteProvider
+     * @param string|null $postalCode
+     * @param string|null $latitude
+     * @param string|null $longitude
+     * @param string|null $accuracyRadius
+     * @param string|null $networkFrom
+     * @param string|null $networkTo
+     */
+    public function __construct(string $network, string $geonameId, ?string $registeredCountryGeonameId,
+                                ?string $representedCountryGeonameId, ?string $isAnonymousProxy,
+                                ?string $isSatelliteProvider, ?string $postalCode, ?string $latitude,
+                                ?string $longitude, ?string $accuracyRadius, ?string $networkFrom, ?string $networkTo)
+    {
+        $this->network = $network;
+        $this->geonameId = $geonameId;
+        $this->registeredCountryGeonameId = $registeredCountryGeonameId;
+        $this->representedCountryGeonameId = $representedCountryGeonameId;
+        $this->isAnonymousProxy = $isAnonymousProxy;
+        $this->isSatelliteProvider = $isSatelliteProvider;
+        $this->postalCode = $postalCode;
+        $this->latitude = $latitude;
+        $this->longitude = $longitude;
+        $this->accuracyRadius = $accuracyRadius;
+        $this->networkFrom = $networkFrom;
+        $this->networkTo = $networkTo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNetwork(): string
+    {
+        return $this->network;
+    }
+
+    /**
+     * @param string $network
+     */
+    public function setNetwork(string $network): void
+    {
+        $this->network = $network;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGeonameId(): string
+    {
+        return $this->geonameId;
+    }
+
+    /**
+     * @param string $geonameId
+     */
+    public function setGeonameId(string $geonameId): void
+    {
+        $this->geonameId = $geonameId;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getRegisteredCountryGeonameId(): ?string
+    {
+        return $this->registeredCountryGeonameId;
+    }
+
+    /**
+     * @param string|null $registeredCountryGeonameId
+     */
+    public function setRegisteredCountryGeonameId(?string $registeredCountryGeonameId): void
+    {
+        $this->registeredCountryGeonameId = $registeredCountryGeonameId;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getRepresentedCountryGeonameId(): ?string
+    {
+        return $this->representedCountryGeonameId;
+    }
+
+    /**
+     * @param string|null $representedCountryGeonameId
+     */
+    public function setRepresentedCountryGeonameId(?string $representedCountryGeonameId): void
+    {
+        $this->representedCountryGeonameId = $representedCountryGeonameId;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getIsAnonymousProxy(): ?string
+    {
+        return $this->isAnonymousProxy;
+    }
+
+    /**
+     * @param string|null $isAnonymousProxy
+     */
+    public function setIsAnonymousProxy(?string $isAnonymousProxy): void
+    {
+        $this->isAnonymousProxy = $isAnonymousProxy;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getIsSatelliteProvider(): ?string
+    {
+        return $this->isSatelliteProvider;
+    }
+
+    /**
+     * @param string|null $isSatelliteProvider
+     */
+    public function setIsSatelliteProvider(?string $isSatelliteProvider): void
+    {
+        $this->isSatelliteProvider = $isSatelliteProvider;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPostalCode(): ?string
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * @param string|null $postalCode
+     */
+    public function setPostalCode(?string $postalCode): void
+    {
+        $this->postalCode = $postalCode;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLatitude(): ?string
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @param string|null $latitude
+     */
+    public function setLatitude(?string $latitude): void
+    {
+        $this->latitude = $latitude;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLongitude(): ?string
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param string|null $longitude
+     */
+    public function setLongitude(?string $longitude): void
+    {
+        $this->longitude = $longitude;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAccuracyRadius(): ?string
+    {
+        return $this->accuracyRadius;
+    }
+
+    /**
+     * @param string|null $accuracyRadius
+     */
+    public function setAccuracyRadius(?string $accuracyRadius): void
+    {
+        $this->accuracyRadius = $accuracyRadius;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getNetworkFrom(): ?string
+    {
+        return $this->networkFrom;
+    }
+
+    /**
+     * @param string|null $networkFrom
+     */
+    public function setNetworkFrom(?string $networkFrom): void
+    {
+        $this->networkFrom = $networkFrom;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getNetworkTo(): ?string
+    {
+        return $this->networkTo;
+    }
+
+    /**
+     * @param string|null $networkTo
+     */
+    public function setNetworkTo(?string $networkTo): void
+    {
+        $this->networkTo = $networkTo;
+    }
+}
